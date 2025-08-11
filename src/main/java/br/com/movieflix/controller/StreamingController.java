7c6d6f838ab781b4cf1081cf39d1e936c548a32c
Guilherme Fontana <guifontana07@gmail.com>
2025-08-11 18:32:54 -0300

    Adicionando o Controller de Streaming, corrigindo palavras, e adicionando nova tabela

--- src/main/java/br/com/movieflix/controller/StreamingController.java
+++ src/main/java/br/com/movieflix/controller/StreamingController.java
@@ -1,19 +1,59 @@
 package br.com.movieflix.controller;
 
+
+import br.com.movieflix.controller.request.StreamingRequest;
+import br.com.movieflix.controller.response.StreamingResponse;
+import br.com.movieflix.entity.Streaming;
+import br.com.movieflix.mapper.StreamingMapper;
 import br.com.movieflix.service.StreamingService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movieflix/streaming")
 public class StreamingController {
 
-    private final StreamingService service;
+    private final StreamingService streamingService;
 
     public StreamingController(StreamingService service) {
-        this.service = service;
+        this.streamingService = service;
     }
 
+    @GetMapping()
+    public ResponseEntity<List<StreamingResponse>> getAllStreaming() {
+        List<StreamingResponse> streams = streamingService.findAll()
+                .stream()
+                .map(StreamingMapper::toStreamingResponse)
+                .toList();
+
+        return ResponseEntity.ok(streams);
+    }
+
+
+    @PostMapping()
+    public ResponseEntity<StreamingResponse> saveStreaming(@RequestBody StreamingRequest request) {
+        Streaming newStreaming = StreamingMapper.toStreaming(request);
+        Streaming savedStreaming = streamingService.save(newStreaming);
+        return ResponseEntity.status(HttpStatus.CREATED).body(StreamingMapper.toStreamingResponse(savedStreaming));
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StreamingResponse> getStreamingId(@PathVariable Long id) {
+        return streamingService
+                .gettByCategoryId(id)
+                .map(category -> ResponseEntity.ok(StreamingMapper.toStreamingResponse(category)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        streamingService.deletar(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
 
 
--- src/main/java/br/com/movieflix/entity/Streaming.java
+++ src/main/java/br/com/movieflix/entity/Streaming.java
@@ -10,7 +10,7 @@ import lombok.*;
 @Getter
 @Setter
 @Entity
-@Table (name = "streming")
+@Table (name = "streaming")
 public class Streaming {
 
 
